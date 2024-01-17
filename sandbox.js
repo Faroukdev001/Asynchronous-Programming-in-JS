b60cbab08a45438efebdf132559512615866e054
Al-Farouk <faroukarogundade@gmail.com>
2024-01-17 22:02:15 +0100

    improved logic

--- sandbox.js
+++ sandbox.js
@@ -1,66 +1,46 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
+  const request = new XMLHttpRequest();
 
-    const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    // console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
 
-    request.addEventListener("readystatechange", () => {
-        // console.log(request, request.readyState);
-        if(request.readyState === 4  && request.status === 200) {
-            callback(undefined, request.responseText);
-        }
-        else if (request.readyState === 4) {
-            callback("could not fetch data", undefined);
-        }
-    });
-
-    request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
-    request.send();
+  request.open("GET", resource);
+  request.send();
 };
 
-console.log(1);
-console.log(2);
+//promise example
 
-getTodos((err, data) => {
-    console.log("callback fired");
-    if (err) {
-        console.log(err)
-    }
-    else {
-        console.log(data);
-    }
-});
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         //fetch something
+//         resolve('some data');
+//         //reject('some error');
+//     });
+// };
 
-console.log(3);
-console.log(4);
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
 
 //const sum = function (a,b) {
-    //     return a+b;
-    // }
-    // console.log(sum(4,5));
-    
-    
-    // const sum1 = (a,b) => a+b;
-    // console.log(sum1(4,5));
+//     return a+b;
+// }
+// console.log(sum(4,5));
+
+// const sum1 = (a,b) => a+b;
+// console.log(sum1(4,5));
